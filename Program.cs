ce04f48a602cced635ad034fa33ccef0d3ad7979
BryAnv466 <60750916@continental.edu.pe>
2026-09-24 11:50:55 -0500

    Hola

--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy( policity =>
+            {
+                policity
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+            }
+        )
+    }
+)
+
 var app = builder.Build();
+
+app.UseCors()
 
 app.MapGet("/",() =>
 {
-    return "Polleria Travis Scott conectada";
+    return "API Polleria funcionando";
 });
 
 app.MapGet("/api/polleria",() =>
 {
     return Results.Ok(new[]
     {
-        new {
+        new{
             id=1,
             codigo="P001",
-            nombre="Pollo a la Brasa",
+            nombre="Pollo a la brasa",
         },
-        new {
+        new{
             id=2,
             codigo="P002",
-            nombre="Pollo Broaster",
+            nombre="Pollo broaster",
         }
     });
 });
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("Port")??"10000",
+app.Run($"http://0.0.0.0:{port}");
